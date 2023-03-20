17616dcaeaea89b5b346c440d2ca6155d7589f1c
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-19 19:15:07 -0700

    good

--- discovery/server/firebase_auth.py
+++ discovery/server/firebase_auth.py
@@ -1,7 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import auth  # Todo
 
 
 cred = credentials.Certificate("./env/firebase_admin_sdk.json")
-default_app = firebase_admin.initialize_app()
+default_app = firebase_admin.initialize_app(cred)
