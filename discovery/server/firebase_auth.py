e48f374e98452f3e2a8739f99ff3b02404c71b35
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-19 19:28:03 -0700

    good

--- discovery/server/firebase_auth.py
+++ discovery/server/firebase_auth.py
@@ -2,5 +2,6 @@ import firebase_admin
 from firebase_admin import credentials
 
 
-cred = credentials.Certificate("./env/firebase_admin_sdk.json")
-default_app = firebase_admin.initialize_app(cred)
+def init_firebase_admin():
+    cred = credentials.Certificate("./env/firebase_admin_sdk.json")
+    firebase_admin.initialize_app(cred)
