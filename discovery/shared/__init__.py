8ba73420fdb081700f191092d509a3ed066a07f5
Haohan Wang <92068838+wayne-wang-1119@users.noreply.github.com>
2023-04-03 21:28:00 -0700

    adding spice specific output

--- discovery/shared/__init__.py
+++ discovery/shared/__init__.py
@@ -29,7 +29,31 @@ class SecretSpiceSimulationOutput:
 
     id: float  # Id (A)
 
+@dataclass
+class TestSimInput:
+    """# Input for Circuit Module to be built in HDL21"""
 
+    def __init__(self, new_name, new_i, new_o, new_s):
+        self.name = new_name
+        self.i = new_i
+        self.o = new_o
+        self.s = new_s
+
+@dataclass
+class TestOutput:
+    """# Output for Circuit Module to be built in HDL21"""
+
+    def __init__(self, module_input):
+        self.m = h.Module(name=module_input.name)
+        self.i = h.Input()
+        self.o = h.Output(width=module_input.o)
+        self.s = h.Signal()
+
+    def tostring(self):
+        return (f'Type: TestModuleOutput\n'
+                f'Attributes:\n'
+                f'  Name: {self.m}\n'
+                f'  o: {self.o}')
 
 @dataclass
 class SpiceInput:
@@ -46,14 +70,15 @@ class SpiceInput:
 class SpiceOutput:
     """# Output for spice in hdl21"""
 
-    def __init__(self, module_input):
-        self.m = h.Module(name=module_input.name)
+    def __init__(self, sim_input):
+        self.r = h.Sim(pkg = sim_input.pkg, top = sim_input.top, opts = sim_input.opts, an = sim_input.an, ctrls = sim_input.ctrls)
         self.i = h.Input()
         self.o = h.Output(width=module_input.o)
         self.s = h.Signal()
 
     def tostring(self):
-        return (f'Type: TestModuleOutput\n'
+        return (f'Type: TestSpiceSimOutput\n'
                 f'Attributes:\n'
                 f'  Name: {self.m}\n'
+                f'  SpiceOutput: {self.r}\n'
                 f'  o: {self.o}')
