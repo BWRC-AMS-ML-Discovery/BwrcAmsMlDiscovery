1e4303e8dbdb4a6c1766536f77a5cb909dcfc8f4
Haohan Wang <92068838+wayne-wang-1119@users.noreply.github.com>
2023-04-16 19:00:25 -0700

    added user time checking

--- discovery/server/__init__.py
+++ discovery/server/__init__.py
@@ -54,6 +54,7 @@ from firebase_admin import auth
 from firebase_admin._auth_utils import InvalidIdTokenError
 from ..shared import WhoAmIInput, WhoAmIOutput
 from firebase_admin import firestore
+db = firestore.client()
 
 init_firebase_admin()
 
@@ -64,10 +65,15 @@ async def whoami(
 ) -> None:
     current_user = None
     
-    db = firestore.client()
+    # access database, if input user exist, return stored info in the database
     user_doc = db.collection('users').document(inp.api_key).get()
-    if user_doc.exists:
-        return WhoAmIOutput(current_user=user_doc.to_dict())
+    if user_doc:
+        user_info = user_doc.to_dict()
+
+
+    if user_info:
+        user_exp = datetime.strptime(user_info['exp'], '%a, %d %b %Y %H:%M:%S GMT')
+        
 
     try:
         current_user = auth.verify_id_token(inp.api_key)
