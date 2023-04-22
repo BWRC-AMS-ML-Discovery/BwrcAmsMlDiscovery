5b038e623b37e332df4d5b417a74ad10fae5fcd9
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-04-22 09:30:02 -0700

    ok

--- discovery/client/auth/__init__.py
+++ discovery/client/auth/__init__.py
@@ -9,6 +9,7 @@ import httpx
 
 # Local Imports
 from . import env
+from ._map import inp_auth_types, out_types
 from discovery.shared.auth import AuthKey
 
 
@@ -19,13 +20,11 @@ def authenticated_request(
     httpx_request_type: Callable[..., httpx.Response],
     server_url: str,
     path: str,
-    inp,
-    inp_auth_type: type,
-    out_type: type,
+    inp=None,
 ):
     """
     TODO Redundant information in parameters
     """
-    inp_auth = inp_auth_type(inp=inp, auth_key=AuthKey(_token))
+    inp_auth = inp_auth_types[path](inp=inp, auth_key=AuthKey(_token))
     resp = httpx_request_type(f"http://{server_url}/{path}", json=asdict(inp_auth))
-    return out_type(**resp.json())
+    return out_types[path](**resp.json())
