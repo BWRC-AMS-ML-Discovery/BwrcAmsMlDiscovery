f6abbdceb318a545fba4aab936289f8c7424ed44
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-04-22 08:16:24 -0700

    could be

--- discovery/server/auth/firebase/provider.py
+++ discovery/server/auth/firebase/provider.py
@@ -1,2 +1,14 @@
-def verify_auth_key() -> bool:
-    return True
+# Firebase Imports
+from firebase_admin import auth
+
+
+# Local Imports
+from ..provider import User
+from discovery.shared.auth import AuthKey
+
+
+def verify_auth_key(auth_key: AuthKey) -> User:
+    user = auth.verify_id_token(auth_key.token)
+
+    # TODO
+    return User(user)
--- discovery/server/auth/provider.py
+++ discovery/server/auth/provider.py
@@ -1,2 +1,15 @@
-def verify_auth_key() -> bool:
-    return True
+from dataclasses import dataclass
+
+
+# Local Imports
+from discovery.shared.auth import AuthKey
+from . import firebase
+
+
+@dataclass
+class User:
+    username: str
+
+
+def verify_auth_key(auth_key: AuthKey) -> User:
+    return firebase.verify_auth_key(auth_key)
