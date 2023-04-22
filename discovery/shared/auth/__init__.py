41901b3f806add9f3ba6b3c4a3059ff0a1bd7585
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-04-22 15:59:43 -0700

    ok

--- discovery/shared/auth/__init__.py
+++ discovery/shared/auth/__init__.py
@@ -3,6 +3,9 @@
 """
 
 
+from typing import Any
+
+
 # Local Imports
 from ..dataclasses import dataclass
 
@@ -10,3 +13,9 @@ from ..dataclasses import dataclass
 @dataclass
 class AuthKey:
     token: str
+
+
+@dataclass
+class AuthenticatedInput:
+    inp: Any | None  # TODO type hint a dataclass
+    auth_key: AuthKey
