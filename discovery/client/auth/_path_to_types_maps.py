4d83a2c0ce800896db83fe14d60a5e6c1e9e40af
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-04-22 15:36:24 -0700

    del

--- discovery/client/auth/_path_to_types_maps.py
+++ discovery/client/auth/_path_to_types_maps.py
@@ -2,10 +2,6 @@ from discovery.shared.auth import inputs
 from discovery.shared import user
 
 
-inp_auth_types = {
-    "whoami": inputs.WhoAmIInputAuth,
-}
-
 out_types = {
     "whoami": user.WhoAmIOutput,
 }
