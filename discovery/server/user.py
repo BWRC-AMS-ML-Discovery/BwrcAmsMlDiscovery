737902e7503dafc260ecfbc9cd660b3598f0e316
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-04-22 16:51:16 -0700

    cool

--- discovery/server/user.py
+++ discovery/server/user.py
@@ -5,7 +5,7 @@ from fastapi import Body
 # Local Imports
 from . import app
 from ..shared.user import WhoAmIOutput
-from ..shared.auth import AuthError, AuthenticatedInput
+from ..shared.auth import AuthError, AuthenticatedInput, AuthenticatedOutput
 
 from .auth.provider import verify_auth_key
 
@@ -13,13 +13,19 @@ from .auth.provider import verify_auth_key
 @app.post("/whoami")
 async def whoami(
     inp: AuthenticatedInput = Body(...),
-) -> WhoAmIOutput | AuthError:
+) -> AuthenticatedOutput:
     try:
         user = verify_auth_key(inp.auth_key)
     except Exception as e:
-        return AuthError(err=str(e))
+        return AuthenticatedOutput(
+            out=None,
+            auth_err=AuthError(err=str(e)),
+        )
 
-    return WhoAmIOutput(
-        username=user.username,
-        email=user.email,
+    return AuthenticatedOutput(
+        out=WhoAmIOutput(
+            username=user.username,
+            email=user.email,
+        ),
+        auth_err=None,
     )
