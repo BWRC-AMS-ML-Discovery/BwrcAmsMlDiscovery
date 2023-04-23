5451824ea6e1df9374865ff0c0f33cb5b21ce26d
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-04-22 19:00:19 -0700

    move

--- discovery/server/mock/__init__.py
+++ discovery/server/mock/__init__.py
@@ -1,58 +1,10 @@
-# stblib
-from typing import Any, Callable
-
-
-# FastAPI Imports
-from fastapi import Body
-
-
 # Local Imports
 from discovery.server import app
+from discovery.server.auth import authenticated_service
 from discovery.shared.mock import (
     MockInverterBetaRatioInput,
     MockInverterBetaRatioOutput,
 )
-from discovery.shared.auth import AuthError, AuthenticatedInput, AuthenticatedOutput
-from discovery.shared.path import path_to_inp_types
-
-from discovery.server.auth.provider import verify_auth_key
-
-
-def authenticated_service(
-    fastapi_request_type: Callable[
-        ..., Callable[[Callable[..., Any]], Callable[..., Any]]
-    ],
-    path: str,
-    **kwargs,
-):
-    def decorator(func):
-        @fastapi_request_type(path, **kwargs)
-        async def authenticated_func(
-            inp_auth: AuthenticatedInput = Body(...),
-        ) -> AuthenticatedOutput:
-            # User auth
-            try:
-                user = verify_auth_key(inp_auth.auth_key)
-            except Exception as e:
-                return AuthenticatedOutput(
-                    out=None,
-                    auth_err=AuthError(err=str(e)),
-                )
-
-            # input
-            inp = path_to_inp_types["/mock/inverter_beta_ratio"](
-                **inp_auth.inp,
-            )
-
-            # output
-            return AuthenticatedOutput(
-                out=func(inp),
-                auth_err=None,
-            )
-
-        return authenticated_func
-
-    return decorator
 
 
 @authenticated_service(app.post, "/mock/inverter_beta_ratio")
