1cc95a49d87c569f17116c8c2b128d43f652fa71
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-08 05:25:50 +0000

    mimic

--- scripts/client_0607_test.py
+++ scripts/client_0607_test.py
@@ -2,9 +2,16 @@ from example_client import test_auto_ckt
 
 
 def main():
-    result = test_auto_ckt()
+    reinforcement_learning_env = {"steps": 5, "update": None}
 
-    print(f"{result=}")
+    for reinforcement_learning_step in range(
+        reinforcement_learning_env["steps"],
+    ):
+        result = test_auto_ckt()
+
+        print(f"{result=}")
+
+        reinforcement_learning_env["update"] = result
 
 
 if __name__ == "__main__":
