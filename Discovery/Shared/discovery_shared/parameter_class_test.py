d17e8c48d41f76b9909b556e7293bb034374c35d
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-20 19:45:11 +0000

    nit

--- Discovery/Shared/discovery_shared/parameter_class_test.py
+++ Discovery/Shared/discovery_shared/parameter_class_test.py
@@ -1,13 +1,28 @@
-from Parameters.parameters import Parameters
+from os import path
 
-test_parameters_object = Parameters("test.yaml")
+
+from .Parameters.parameters import Parameters
+
+
+# Get the path to this file
+file_path = path.dirname(path.abspath(__file__))
+# Get the path to the yaml file
+yaml_path = path.join(file_path, "test.yaml")
+
+test_parameters_object = Parameters(yaml_path)
 
 print(test_parameters_object)
 
 test_parameters_object.set_parameter("generator_type", "BAG")
-print("New generator type:", test_parameters_object.get_parameter("generator_type"))
+print(
+    "New generator type:",
+    test_parameters_object.get_parameter("generator_type"),
+)
 
 test_parameters_object.set_hyperparameter("discount_factor", 0.25)
-print("New discount factor:", test_parameters_object.get_hyperparameter("discount_factor"))
+print(
+    "New discount factor:",
+    test_parameters_object.get_hyperparameter("discount_factor"),
+)
 
 print(test_parameters_object)
