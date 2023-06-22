d2c84f0d3ef638c35e4c79980b45aa719fd8a01b
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-22 22:55:20 +0000

    something like this

--- AutoCkt/scripts/train_general.py
+++ AutoCkt/scripts/train_general.py
@@ -1,16 +1,27 @@
-from autockt import AutoCktTrainer
-from eval_engines import ExampleOpAmpEvalEngine
+from autockt import AutoCktTrainer, derive_action_space
+
+# eval_engines will be renamed to something like
+from eval_engines import (
+    ExampleOpAmpParams,
+    ExampleOpAmpSpecs,
+    # ExampleOpAmpActionSpace, # Suppose we want to derive this
+    ExampleOpAmpObservationSpace,
+    ExampleOpAmpReward,
+)
 
 
 def main():
-    # Create the evaluation engine
-    eval_engine = ExampleOpAmpEvalEngine()
-
     # Create the trainer
-    trainer = AutoCktTrainer(eval_engine)
+    trainer = AutoCktTrainer()
 
     # Train the model
-    agent = trainer.train()
+    agent = trainer.train(
+        params=ExampleOpAmpParams,
+        specs=ExampleOpAmpSpecs,
+        action_space=derive_action_space(ExampleOpAmpParams),  # Derive the action space
+        observation_space=ExampleOpAmpObservationSpace,
+        reward=ExampleOpAmpReward,
+    )
 
     # Save the model
     agent.save()
