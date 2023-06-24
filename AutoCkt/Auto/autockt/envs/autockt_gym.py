a1a20c5ab9766921335e38e8d0582f50d0b05f27
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-24 19:53:22 +0000

    so far

--- AutoCkt/Auto/autockt/envs/autockt_gym.py
+++ AutoCkt/Auto/autockt/envs/autockt_gym.py
@@ -1,12 +1,15 @@
 # PyPI imports
 import gym
 
+# Local imports
+from shared.autockt_gym_env_config import AutoCktGymEnvConfig
+
 
 class AutoCktGym(gym.Env):
     metadata = {
         "render.modes": ["human"],
     }
 
-    def __init__(self, env_config):
+    def __init__(self, env_config: AutoCktGymEnvConfig):
         # Custom attributes (not from gym.Env)
         pass
