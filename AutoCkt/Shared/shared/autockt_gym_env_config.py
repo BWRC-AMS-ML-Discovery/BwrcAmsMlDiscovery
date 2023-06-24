24e80298b14513d36be3d41fbe5920e1f74b469a
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-24 20:30:55 +0000

    fine

--- AutoCkt/Shared/shared/autockt_gym_env_config.py
+++ AutoCkt/Shared/shared/autockt_gym_env_config.py
@@ -1,6 +1,39 @@
+# Stdlib imports
+from typing import Callable
+
+# PyPI imports
 from pydantic.dataclasses import dataclass
+
+
+Number = int | float
+
+
+@dataclass
+class Range:
+    min: Number
+    max: Number
+
+
+@dataclass
+class Param:
+    name: str
+    range: Range
+
+
+@dataclass
+class Params:
+    pass
+
+
+@dataclass
+class Specs:
+    pass
 
 
 @dataclass
 class AutoCktGymEnvConfig:
-    pass
+    params: Params
+    specs: Specs
+    input_type: type
+    output_type: type
+    reward: Callable[[], float]
