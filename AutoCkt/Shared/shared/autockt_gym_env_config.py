9fd192828f9869aa44bc9499f0f8591d0c948d43
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-24 20:51:03 +0000

    comment

--- AutoCkt/Shared/shared/autockt_gym_env_config.py
+++ AutoCkt/Shared/shared/autockt_gym_env_config.py
@@ -42,4 +42,10 @@ class AutoCktGymEnvConfig:
     specs: AutoCktSpecs
     input_type: type
     output_type: type
-    reward: Callable[[TODO], float]
+    reward: Callable[
+        [AutoCktSpec, AutoCktSpec],
+        # TODO The input to reward can be expanded to (s_t, a_t, s_{t+1}),
+        # as defined in OpenAI's Spinning Up intro to RL:
+        # https://spinningup.openai.com/en/latest/spinningup/rl_intro.html#reward-and-return
+        float,
+    ]
