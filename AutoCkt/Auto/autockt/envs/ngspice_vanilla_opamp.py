3e2ced0ac7ac84a02c6ae36aaf19c25a061ac3ec
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-25 01:39:35 +0000

    back?

--- AutoCkt/Auto/autockt/envs/ngspice_vanilla_opamp.py
+++ AutoCkt/Auto/autockt/envs/ngspice_vanilla_opamp.py
@@ -4,7 +4,6 @@ A new ckt environment based on a new structure of MDP
 
 # Std-Lib Imports
 import os, pickle, random
-from dataclasses import fields
 
 # from collections import OrderedDict
 
@@ -15,7 +14,7 @@ from gym import spaces
 
 # Workspace Imports
 from .create_design_and_simulate_lib import create_design_and_simulate
-from shared import Range, CktInput, Params, Normalize, TargetSpecs, Spec
+from shared import *
 from eval_engines import PARAMS_RANGE, NORM_CONSTANT, TARGET_RANGE
 
 # FIXME Avoid storing files?
