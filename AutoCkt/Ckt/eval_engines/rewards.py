8c35d0fedefd42a12e7a9d5a14ec420fa53d5a4a
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-25 03:00:46 +0000

    like this

--- AutoCkt/Ckt/eval_engines/rewards.py
+++ AutoCkt/Ckt/eval_engines/rewards.py
@@ -2,15 +2,18 @@ from example_client import AutoCktOutput
 from shared.typing import Number
 
 
-def settaluri_reward(curr_specs: AutoCktOutput, ideal_specs: dict[str, Number]):
+def settaluri_reward(
+    curr_output: AutoCktOutput,  # This is from simulation
+    target_output: dict[str, Number],  # This is from Specs
+):
     """
     Reward: doesn't penalize for overshooting spec, is negative
     """
 
-    def calc_relative_spec(spec: float, ideal_spec: float):
-        ideal_spec = float(ideal_spec)  # Not sure if this is necessary
-        relative_spec = (spec - ideal_spec) / (ideal_spec + spec)
-        return relative_spec
+    def calc_relative(curr: float, ideal: float):
+        ideal = float(ideal)  # Not sure if this is necessary
+        relative = (curr - ideal) / (curr + ideal)
+        return relative
 
     # FIXME
     raise NotImplementedError
