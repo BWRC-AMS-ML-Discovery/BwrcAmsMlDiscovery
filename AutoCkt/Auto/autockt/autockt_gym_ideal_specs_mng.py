d35e3bd96c9cae989c7d357b59bc099ff99f55cd
cythirixs <Cythirixs@github.com>
2023-06-29 19:03:26 +0000

    comments

--- AutoCkt/Auto/autockt/autockt_gym_ideal_specs_mng.py
+++ AutoCkt/Auto/autockt/autockt_gym_ideal_specs_mng.py
@@ -33,7 +33,12 @@ class SpecManager:
     # ideal norm is calculate from ideal_spec and normalized values
     ideal_norm: dict[str:Number]
 
-    def __init__(self, init_spec):
+    def __init__(self, init_spec: AutoCktSpecs):
+        """
+        takes init_spec : AutoCktSpecs
+
+        generates initial variable
+        """
         self.init_spec = init_spec
 
         self.spec_id = [spec.str for spec in init_spec]
@@ -41,13 +46,25 @@ class SpecManager:
         self.ideal_norm = self.normalize(self.ideal_spec)
         self.cur_spec = np.zeroes(len(self.spec_id))
 
-    def step(self, params):
+    def step(self, params: dict[str:Number]):
+        """
+        Takes a dict of param values and updates the current spec values
+
+        returns the current spec, ideal_spec, the norm of the current spec, and the norm of the ideal spec
+        returns the current and ideal spec so that reward can be calculated
+        """
         self.cur_spec = self.update(params)
         cur_norm = self.normalize(self.cur_spec)
 
-        return [self.cur_spec, cur_norm, self.ideal_norm]
+        return [self.cur_spec, self.ideal_spec, cur_norm, self.ideal_norm]
 
     def reset(self, params: dict[str:Number]):
+        """
+        Takes a dict of param values to reset the current spec value to
+        generates a new set of ideal specs
+
+        returns the normalied values of the current spec and the ideal spec
+        """
         self.ideal_spec = self.gen_spec()
         self.ideal_norm = self.normalize(self.ideal_spec)
 
@@ -56,12 +73,18 @@ class SpecManager:
 
         return [cur_norm, self.ideal_norm]
 
-    def update(self, params: dict[str:Number]):
+    def update(self, params: dict[str:Number]) -> dict[str:Number]:
+        """
+        simulates on the given param values and returns a spec dict
+        """
         simulated = create_design_and_simulate(params)
         return simulated
 
-    def normalize(self, spec):
-        to_normalize = spec.values()
+    def normalize(self, specs: dict[str:Number]) -> dict[str:Number]:
+        """
+        given a dict of specs, calculate and return normalized spec value
+        """
+        to_normalize = specs.values()
         normalize = [spec.normalize for spec in self.init_spec]
 
         relative = (to_normalize - normalize) / (to_normalize + normalize)
@@ -71,7 +94,7 @@ class SpecManager:
 
     def gen_spec(self):
         """
-        using the given range, randomly generate one set of specs which fits this range
+        using the given range from init_spec, randomly generate one set of specs which fits this range
         """
         spec_values = []
         for spec in self.init_spec:
