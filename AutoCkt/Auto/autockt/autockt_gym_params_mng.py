9aedd7c13e74460654b7022af0fe462b9286244a
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-11 22:24:09 +0000

    minor fix

--- AutoCkt/Auto/autockt/autockt_gym_params_mng.py
+++ AutoCkt/Auto/autockt/autockt_gym_params_mng.py
@@ -11,16 +11,18 @@ class AutoCktParamsManager:
         params_init_dict = {param.name: param.init for param in self.params_ranges}
         self.cur_params = params_init_dict
 
-    def step(self, cur_action: list[Number]):
+    def step(self, cur_action):
         """based on action space move by action's idx"""
         # Convert to a numpy array and flatten it if it's not already 1D
         for idx, (name, _) in enumerate(self.cur_params.items()):
             step_update = self.cur_params[name] + cur_action[name]
-            if (
-                step_update <= self.params_ranges[idx].range.max
-                and step_update >= self.params_ranges[idx].range.min
-            ):
-                self.cur_params[name] = step_update
+
+            if step_update > self.params_ranges[idx].range.max:
+                step_update = self.params_ranges[idx].range.max
+            elif step_update < self.params_ranges[idx].range.min:
+                step_update = self.params_ranges[idx].range.min
+
+            self.cur_params[name] = step_update
 
     def get_cur_params(self) -> dict[str, Number]:
         return self.cur_params
