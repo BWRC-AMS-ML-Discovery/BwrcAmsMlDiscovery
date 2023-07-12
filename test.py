86d7bf866c96ca942e8b81f47aa7efe5ed6d1375
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-12 01:12:24 +0000

    test auto_ckt_sim_hdl21

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-from example_server import auto_ckt_sim, AutoCktInput, AutoCktOutput  # FIXME: move
+from example_client import (
+    auto_ckt_sim_hdl21,
+    AutoCktInput,
+    AutoCktOutput,
+)
 
 inp = AutoCktInput(3, 3, 3, 3, 3, 3, 1e-12)
-out = auto_ckt_sim_Hdl21(inp)
+out = auto_ckt_sim_hdl21(inp)
 print(out)
