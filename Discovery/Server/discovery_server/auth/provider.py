e8f504517a547a72effb270fdf08bb722cff860a
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-13 01:48:34 +0000

    revert

--- Discovery/Server/discovery_server/auth/provider.py
+++ Discovery/Server/discovery_server/auth/provider.py
@@ -8,11 +8,4 @@ from .firebase import provider
 
 
 def verify_auth_key(auth_key: AuthKey) -> User:
-    return User(
-        name="FIXME",
-        email="FIXME",
-        exp=None,
-    )
-
-    # FIXME Fix this Firebase
     return provider.verify_auth_key(auth_key)
