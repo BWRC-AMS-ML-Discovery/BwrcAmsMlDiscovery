2862b1417e7d156ba1d7e93c543469e4cb2e0f85
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-13 02:10:36 +0000

    works!

--- Discovery/Server/discovery_server/authentication/__init__.py
+++ Discovery/Server/discovery_server/authentication/__init__.py
@@ -1,6 +1,17 @@
-from firebase_admin import auth
+import os
+
+import firebase_admin
+from firebase_admin import auth, credentials
 
 from .user import User
+
+
+# Get current file directory
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+_cred = credentials.Certificate(DIR_PATH + "/firebase_admin_sdk.json")
+firebase_admin.initialize_app(_cred)
 
 
 def verify_credentials(credentials) -> User:
