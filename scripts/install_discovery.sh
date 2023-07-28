c797ed5b3a5036659acf748e50892419f22f6fb4
wayne-wang-1119 <whh.bear@berkeley.edu>
2023-07-28 22:22:22 +0000

    sample server ready

---
+++ scripts/install_discovery.sh
@@ -0,0 +1,13 @@
+set -eo
+
+# Install all of our packages with development extras
+pip install \
+    -e "./Discovery/Shared[dev]" \
+    -e "./Discovery/Server[dev]" \
+    -e "./Discovery/Client[dev]" \
+    -e "./Example/Shared[dev]" \
+    -e "./Example/Server[dev]" \
+    -e "./Example/Client[dev]" \
+
+# Set up pre-commit hooks
+pre-commit install
