52d0726b30fc63bb220f2246d611e201522d1abd
wayne-wang-1119 <whh.bear@berkeley.edu>
2023-07-28 22:17:50 +0000

    sample

---
+++ Sample/Client/sample_client/__init__.py
@@ -0,0 +1,138 @@
+# Std-Lib Imports
+from dataclasses import asdict
+
+# PyPi Imports
+from dotenv import dotenv_values
+import hdl21 as h
+
+# Workspace Imports
+from sample_shared import (
+    sample,
+    Sample,
+    secret_spice,
+    SecretSpiceSimulationInput,
+    SecretSpiceSimulationOutput,
+    simulate_that_opamp,
+    OpAmpParams,
+    VlsirProtoBufKind,
+    VlsirProtoBufBinary,
+    inverter_beta_ratio,
+    InverterBetaRatioInput,
+    InverterBetaRatioOutput,
+    simulate_on_the_server,
+    auto_ckt_sim,
+    auto_ckt_sim_hdl21,
+    AutoCktInput,
+    AutoCktOutput,
+)
+
+# The client library will create client stubs for all defined RPCs, including all those functions above.
+import discovery_client as dc
+from discovery_client import Config
+
+
+def sample_client_start(cfg: Config):
+    """retrieve values from .env file then configure nad start the client"""
+
+    dc.configure(cfg)
+
+    if cfg.enable_https:
+        dc.client_start()
+    else:
+        # Short-circuiting by directly calling server functions
+        import sample_server as _
+
+
+"""
+Now we can just call the RPCs as though they were implemented locally.
+"""
+
+
+def test_auto_ckt():
+    """testing auto ckt rpcs"""
+    to_test = AutoCktInput(3, 3, 3, 3, 3, 3, 1e-12)
+    test = auto_ckt_sim(to_test)
+    return test
+
+
+def do_simple_sample():
+    sample_resp = sample(Sample(txt="Hello", num=3))
+    return sample_resp
+
+
+def do_sample_stuff():
+    """# Call a few sample RPCs"""
+    sample_resp = sample(Sample(txt="Hello", num=3))
+    print(sample_resp)
+
+    secret_spice_resp = secret_spice(SecretSpiceSimulationInput(w=1, l=2, v=3))
+    print(secret_spice_resp)
+
+    simulate_that_opamp_resp = simulate_that_opamp(OpAmpParams(nf_something=3))
+    print(simulate_that_opamp_resp)
+
+
+# FIXME needs renaming to avoid the same name
+def simulate_that_opamp(params: OpAmpParams) -> h.sim.SimResultProto:
+    """# Run the `ThatOpAmp` generator and simulate it, all on the server"""
+
+    # Call our RPC function
+    protos: VlsirProtoBufBinary = simulate_that_opamp(params)
+
+    # Got some data back! Decode it from protobuf into a `SimResultProto`.
+    sim_result = h.sim.SimResultProto.ParseFromString(protos.proto_bytes)
+    print(sim_result)
+    return sim_result
+
+
+def elaborate_that_opamp_here_and_simulate_on_the_server(
+    params: OpAmpParams,
+) -> h.sim.SimResultProto:
+    """# Run `ThatOpAmp` generator here, and send the result to the server for simulation"""
+
+    @h.generator
+    def ThatOpAmp(params: OpAmpParams) -> h.Module:
+        """# That OpAmp"""
+
+        @h.module
+        class ThatOpAmp:
+            VDD, VSS = h.Ports(2)
+            n1 = h.Nmos(npar=params.nf_something)(d=VDD, g=VDD, s=VSS, b=VSS)
+
+        return ThatOpAmp
+
+    @h.module
+    class Tb:
+        VSS = h.Port()
+        VDD = h.Signal()
+        opamp = ThatOpAmp(params=params)(VDD=VDD, VSS=VSS)
+        vdd = h.Vdc(dc=1)(p=VDD, n=VSS)
+
+    @h.sim.sim
+    class OpAmpSim:
+        tb = Tb
+        op = h.sim.Op()
+
+    # Turn it into VLSIR protobuf
+    proto_python_objects = h.sim.to_proto(OpAmpSim)
+    proto_bytes = proto_python_objects.SerializeToString()
+    send_me_to_server = VlsirProtoBufBinary(
+        kind=VlsirProtoBufKind.SIM_INPUT, proto_bytes=proto_bytes
+    )
+
+    # And figure out how to send it to the server
+    from_the_server: VlsirProtoBufBinary = simulate_on_the_server(send_me_to_server)
+
+    # Got some data back! Decode it from protobuf into a `SimResultProto`.
+    sim_result = h.sim.SimResultProto.ParseFromString(from_the_server.proto_bytes)
+    return sim_result
+
+
+def local_inverter_beta_ratio(inp: InverterBetaRatioInput):
+    wp = inp.wp
+    wn = inp.wn
+    output = (wp - 23) ** 2 + (wn - 4) ** 2
+    return InverterBetaRatioOutput(
+        trise=output / 2,
+        tfall=output / 2,
+    )
---
+++ Sample/Server/sample_server/__init__.py
@@ -0,0 +1,237 @@
+"""
+# Sample Server
+"""
+
+# Stdlib Imports
+from dataclasses import asdict
+
+# PyPi Imports
+import hdl21 as h
+import vlsirtools.spice as vsp
+
+import discovery_server as ds
+from discovery_server import Config
+from dotenv import dotenv_values
+
+# Workspace Imports
+from sample_shared import (
+    sample,
+    Sample,
+    secret_spice,
+    SecretSpiceSimulationInput,
+    SecretSpiceSimulationOutput,
+    simulate_that_opamp,
+    simulate_on_the_server,
+    OpAmpParams,
+    VlsirProtoBufKind,
+    VlsirProtoBufBinary,
+    inverter_beta_ratio,
+    InverterBetaRatioInput,
+    InverterBetaRatioOutput,
+    auto_ckt_sim,
+    auto_ckt_sim_hdl21,
+    AutoCktInput,
+    AutoCktOutput,
+)
+from .auto_ckt_sim_lib import (
+    create_design,
+    simulate,
+    translate_result,
+)
+from .mocks.TwoStageOpAmp import (
+    OpAmpParams as TwoStageOpAmpParams,
+    OpAmpSim,
+    find_dc_gain,
+    find_I_vdd,
+    find_phm,
+    find_ugbw,
+)
+
+
+def sample_server_start(cfg: Config):
+    """Retrieve values from .env and then configure and start the server"""
+
+    ds.configure(cfg)
+    ds.start_server()
+
+
+@sample.impl
+def sample_func(sample: Sample) -> Sample:
+    """# Sample RPC"""
+
+    return Sample(txt=sample.txt * sample.num, num=1)
+
+
+@secret_spice.impl
+def secret_spice_sim(inp: SecretSpiceSimulationInput) -> SecretSpiceSimulationOutput:
+    """# Super-secret SPICE simulation"""
+
+    return SecretSpiceSimulationOutput(id=5e-6)
+
+
+@simulate_that_opamp.impl
+async def simulate_that_opamp(params: OpAmpParams) -> VlsirProtoBufBinary:
+    """# Some op-amp simulation"""
+
+    @h.generator
+    def ThatOpAmp(params: OpAmpParams) -> h.Module:
+        """# That OpAmp"""
+
+        @h.module
+        class ThatOpAmp:
+            VDD, VSS = h.Ports(2)
+            n1 = h.Nmos(npar=params.nf_something)(d=VDD, g=VDD, s=VSS, b=VSS)
+
+        return ThatOpAmp
+
+    @h.module
+    class Tb:
+        VSS = h.Port()
+        VDD = h.Signal()
+        opamp = ThatOpAmp(params)(VDD=VDD, VSS=VSS)
+        vdd = h.Vdc(dc=1)(p=VDD, n=VSS)
+
+    @h.sim.sim
+    class OpAmpSim:
+        tb = Tb
+        op = h.sim.Op()
+
+    sim_options = vsp.SimOptions(
+        simulator=vsp.SupportedSimulators.NGSPICE, fmt=vsp.ResultFormat.VLSIR_PROTO
+    )
+
+    # Run that sim!!!
+    somehow_this_is_a_coroutine = await OpAmpSim.run_async(opts=sim_options)
+    ## FIXME: who on earth made this double-await a thing?!?
+    ## Get some better tests in async-land @dan_fritchman
+    sim_result = await somehow_this_is_a_coroutine
+
+    # And bundle it up into our return type
+    return VlsirProtoBufBinary(
+        kind=VlsirProtoBufKind.SIM_RESULT,
+        proto_bytes=sim_result.SerializeToString(),
+    )
+
+
+@simulate_on_the_server.impl
+async def simulate_on_the_server(inp: VlsirProtoBufBinary) -> VlsirProtoBufBinary:
+    """# Simulate a circuit on the server
+    Decodes a `SimInput` VLSIR protobuf from `inp`, simulates it, and returns a `SimResult` VLSIR protobuf.
+    """
+
+    if inp.kind != VlsirProtoBufKind.SIM_INPUT:
+        raise ValueError(f"Expected a simulation input, not {inp.kind}")
+
+    # Got what should be a `SimInput`. First deserialize it from bytes.
+    sim_input = vsp.SimInput.ParseFromString(inp.proto_bytes)
+    if not isinstance(sim_input, vsp.SimInput):
+        raise ValueError(f"Expected a `SimInput`, not {sim_input}")
+
+    sim_options = vsp.SimOptions(
+        simulator=vsp.SupportedSimulators.NGSPICE,  ## or your favorite simulator. or make this part of the input?
+        fmt=vsp.ResultFormat.VLSIR_PROTO,
+    )
+
+    # Finally! Run the simulation!!
+    sim_result = await vsp.spice.sim(sim_input, sim_options)
+    ## FIXME: same "double await" as above
+    sim_result = await sim_result
+
+    # And bundle it up into our return type
+    return VlsirProtoBufBinary(
+        kind=VlsirProtoBufKind.SIM_RESULT,
+        proto_bytes=sim_result.SerializeToString(),
+    )
+
+
+@inverter_beta_ratio.impl
+async def inverter_beta_ratio(inp: InverterBetaRatioInput) -> InverterBetaRatioOutput:
+    """# Super-elaborate inverter beta ratio simulation"""
+    wp = inp.wp
+    wn = inp.wn
+    the_ratio = 1.2
+
+    # TODO implement
+
+    # Mock a paraboloid
+    output = (wp - 3) ** 2 + (wn - 4) ** 2
+    return InverterBetaRatioOutput(
+        trise=output / 2,
+        tfall=output / 2,
+    )
+
+
+# FIXME should be async? FastAPI says both are ok.
+@auto_ckt_sim.impl
+def auto_ckt_sim(inp: AutoCktInput) -> AutoCktOutput:
+    """
+    AutoCkt Simulation
+    """
+    # print(f"input {inp}")
+    tmpdir, design_folder, fpath = create_design(inp)
+
+    # print(f"design created {design_folder}")
+    # TODO Error return?
+    info = simulate(fpath)
+
+    # print(f"simualted {info}")
+
+    specs = translate_result(tmpdir, design_folder)
+    # print(f"to specs {specs}")
+    return specs
+
+
+@auto_ckt_sim_hdl21.impl
+def auto_ckt_sim_hdl21(inp: AutoCktInput) -> AutoCktOutput:
+    """
+    AutoCkt Simulation
+    """
+    if not vsp.ngspice.available():
+        raise RuntimeError
+
+    # Convert our input into `OpAmpParams`
+    # FIXME Is this correct?
+    params = TwoStageOpAmpParams(
+        wp1=inp.mp1,
+        wn1=inp.mn1,
+        wp3=inp.mp3,
+        wn3=inp.mn3,
+        wn4=inp.mn4,
+        wn5=inp.mn5,
+        Cc=inp.cc,
+        # FIXME Extra, don't need?
+        wp2=inp.mp1,
+        wn2=inp.mn1,
+    )
+
+    # Create a set of simulation input for it
+    sim_input = OpAmpSim(params)
+    print(sim_input)
+    print(params)
+
+    # Simulation options
+    opts = vsp.SimOptions(
+        simulator=vsp.SupportedSimulators.NGSPICE,
+        fmt=vsp.ResultFormat.SIM_DATA,  # Get Python-native result types
+        rundir="./scratch",  # Set the working directory for the simulation. Uses a temporary directory by default.
+    )
+
+    # Run the simulation!
+    results = sim_input.run(opts)
+
+    # Extract our metrics from those results
+    ac_result = results["ac"]
+    sig_out = ac_result.data["v(xtop.sig_out)"]
+    gain = find_dc_gain(2 * sig_out)
+    ugbw = find_ugbw(ac_result.freq, 2 * sig_out)
+    phm = find_phm(ac_result.freq, 2 * sig_out)
+    idd = ac_result.data["i(v.xtop.vvdc)"]
+    ibias = find_I_vdd(idd)
+
+    # And return them as an `AutoCktOutput`
+    return AutoCktOutput(
+        ugbw=ugbw,
+        gain=gain,
+        phm=phm,
+        ibias=ibias,
+    )
