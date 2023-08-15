f3d5b01a4644655809065965fc3501df673720d6
Han Yan <yanh41@live.com>
2023-08-15 09:08:57 +0000

    Finished FF parameter extraction

--- Example/Server/example_server/LDO_1.py
+++ Example/Server/example_server/LDO_1.py
@@ -30,6 +30,7 @@ Real versions will have some more parameters; these just have multiplier "m".
 class MosParams:
     m = h.Param(dtype=int, desc="Transistor Multiplier")
 
+
 @h.paramclass
 class PdkMosParams:
     w = h.Param(dtype=h.Scalar, desc="Width in resolution units", default=0.5 * µ)
@@ -53,9 +54,11 @@ pmos = h.ExternalModule(
     spicetype=SpiceType.MOS,
 )
 
+
 @h.paramclass
 class LDOParams:
     """Parameter class"""
+
     w1 = h.Param(dtype=int, desc="Width of m1", default=10)
     w2 = h.Param(dtype=int, desc="Width of m2", default=10)
     w3 = h.Param(dtype=int, desc="Width of m3", default=10)
@@ -64,21 +67,21 @@ class LDOParams:
     w6 = h.Param(dtype=int, desc="Width of m6", default=10)
     w7r = h.Param(dtype=int, desc="Width of m7r", default=10)
     w8 = h.Param(dtype=int, desc="Width of m8", default=10)
-    wc = h.Param(dtype = int, desc = "Width of mc", default = 10)
-    w10 = h.Param(dtype = int, desc = "Width of m10", default = 10)
-    wpass = h.Param(dtype = int, desc = "Width of mpass", default = 10)
+    wc = h.Param(dtype=int, desc="Width of mc", default=10)
+    w10 = h.Param(dtype=int, desc="Width of m10", default=10)
+    wpass = h.Param(dtype=int, desc="Width of mpass", default=10)
     VDD = h.Param(dtype=h.Scalar, desc="VDD voltage", default=1.2)
-    Cc = h.Param(dtype = h.Scalar, desc = "Cc capacitance", default = 1e-11)
+    Cc = h.Param(dtype=h.Scalar, desc="Cc capacitance", default=1e-11)
     Cf1 = h.Param(dtype=h.Scalar, desc="Cf1 capacitance", default=1e-11)
     Cf2 = h.Param(dtype=h.Scalar, desc="Cf2 capacitance", default=1e-11)
-    Rrf1 = h.Param(dtype = h.Scalar, desc = "Rrf1 resistance", default = 1e6)
-    Rrf2 = h.Param(dtype = h.Scalar, desc = "Rrf2 reeistance", default = 4e6)
+    Rrf1 = h.Param(dtype=h.Scalar, desc="Rrf1 resistance", default=1e6)
+    Rrf2 = h.Param(dtype=h.Scalar, desc="Rrf2 reeistance", default=4e6)
     ibias = h.Param(dtype=h.Scalar, desc="ibias current", default=3e-5)
 
 
 @h.generator
 def LDO_1(p: LDOParams) -> h.Module:
-    """# LDO """
+    """# LDO"""
 
     @h.module
     class LDO:
@@ -86,38 +89,38 @@ def LDO_1(p: LDOParams) -> h.Module:
         VDD, VSS = 2 * h.Input()
         ibias = h.Input()
         vref = h.Input()
-        
+
         vout = h.Output()
 
         # Internal Signals
-        v1, v2, v3, v4, v5, v6, vfb = h.Signals(6)
+        v1, v2, v3, v4, v5, v6, vfb = h.Signals(7)
 
         # EA
-        m1 = nmos(m = p.w1)(d = v1, g = vfb, s = v3, b = v3)  # vfb
-        m2 = nmos(m = p.w2)(d = v2, f = vref, s = v3, b = v3)   # vref
-        m3 = pmos(m = p.w3)(d = v1, g = v1, s = VDD, b = VDD) # Current Mirror
-        m4 = pmos(m = p.w4)(d = v2, g = v1, s = VDD, b = VDD) # Current Mirror
-        mc = nmos(m = p.wc)(d = v1, g = vref, s = v4, b = v4)
-        m8 = nmos(m = p.w8)(d = v2, g = vref, s = v5, b = v5)
+        m1 = nmos(m=p.w1)(d=v1, g=vfb, s=v3, b=v3)  # vfb
+        m2 = nmos(m=p.w2)(d=v2, g=vref, s=v3, b=v3)  # vref
+        m3 = pmos(m=p.w3)(d=v1, g=v1, s=VDD, b=VDD)  # Current Mirror
+        m4 = pmos(m=p.w4)(d=v2, g=v1, s=VDD, b=VDD)  # Current Mirror
+        mc = nmos(m=p.wc)(d=v1, g=vref, s=v4, b=v4)
+        m8 = nmos(m=p.w8)(d=v2, g=vref, s=v5, b=v5)
 
         #
-        mpass = pmos(m = p.wpass)(d = vout, g = v2, s = VDD, b = VDD)
+        mpass = pmos(m=p.wpass)(d=vout, g=v2, s=VDD, b=VDD)
 
         # Current Biasing
-        m5 = nmos(m = p.w5)(d = v3, g = v6, s = VSS, b = VSS)   # Bias Current
-        m6 = nmos(m = p.w6)(d = v6, g = v6, s = VSS, b = VSS)   # Current Source Bias
-        m7r = nmos(m = p.w7r)(d = v4, g = v6, s = VSS, b = VSS) 
-        m10 = nmos(m = p.w10)(d = v5, g = v6, s = VSS, b = VSS)
-
+        m5 = nmos(m=p.w5)(d=v3, g=v6, s=VSS, b=VSS)  # Bias Current
+        m6 = nmos(m=p.w6)(d=v6, g=v6, s=VSS, b=VSS)  # Current Source Bias
+        m7r = nmos(m=p.w7r)(d=v4, g=v6, s=VSS, b=VSS)
+        m10 = nmos(m=p.w10)(d=v5, g=v6, s=VSS, b=VSS)
 
         # Compensation Network
-        Cc = h.Cap(c = p.Cc)(p = v5, n = vout) # Miller Capacitance
+        Cc = h.Cap(c=p.Cc)(p=v5, n=vout)  # Miller Capacitance
 
         # Feedback
-        Cf1 = h.Cap(c = p.Cf1)(p = vout, n = vfb)
-        Cf2 = h.Cap(c = p.Cf2)(p = vfb, n = VSS)
-        Rf1 = h.Res(r = p.Rrf1)(p = vout, n = vfb)
-        Rf2 = h.Res(r = p.Rrf2)(p = vfb, n = VSS)
+        Cf1 = h.Cap(c=p.Cf1)(p=vout, n=vfb)
+        Cf2 = h.Cap(c=p.Cf2)(p=vfb, n=VSS)
+        Rf1 = h.Res(r=p.Rrf1)(p=vout, n=vfb)
+        Rf2 = h.Res(r=p.Rrf2)(p=vfb, n=VSS)
+
     return LDO
 
 
@@ -145,12 +148,13 @@ class Compensation:
     r = ResCell(p=a, sub=VDD)
     c = CapCell(p=r.n, n=b, VDD=VDD, VSS=VSS)
 
+
 # FIXME: no sim yet
 @hs.sim
 class MosDcopSim:
     """# Mos Dc Operating Point Simulation Input"""
+
     # def __init__(params):
-        
 
     @h.module
     class Tb:
@@ -161,17 +165,16 @@ class MosDcopSim:
         dcin = h.Diff()
         sig_out = h.Signal()
         i_bias = h.Signal()
-        sig_p = h.Vdc(dc=0.6, ac=0.5)(p=dcin.p,n=VSS)
-        sig_n = h.Vdc(dc=0.6, ac=-0.5)(p=dcin.n,n=VSS)
-        Isource = h.Isrc(dc = 3e-5)(p = vdc.p, n = i_bias)
-        
-        inst=LDO_1()(VDD=vdc.p, VSS=VSS, ibias=i_bias, inp=dcin, out=sig_out)
+        sig_p = h.Vdc(dc=0.6, ac=0.5)(p=dcin.p, n=VSS)
+        sig_n = h.Vdc(dc=0.6, ac=-0.5)(p=dcin.n, n=VSS)
+        Isource = h.Isrc(dc=3e-5)(p=vdc.p, n=i_bias)
+
+        inst = LDO_1()(VDD=vdc.p, VSS=VSS, ibias=i_bias, inp=dcin, out=sig_out)
 
     # Simulation Stimulus
     op = hs.Op()
     ac = hs.Ac(sweep=hs.LogSweep(1e1, 1e10, 10))
     mod = hs.Include("../45nm_bulk.txt")
-
 
 
 def main():
@@ -189,19 +192,30 @@ def main():
     # Run the simulation!
     results = MosDcopSim.run(opts)
 
+    print(
+        "Gain:            "
+        + str(find_dc_gain(2 * results["ac"].data["v(xtop.sig_out)"]))
+    )
+    print(
+        "UGBW:            "
+        + str(find_ugbw(results["ac"].freq, 2 * results["ac"].data["v(xtop.sig_out)"]))
+    )
+    print(
+        "Phase margin:    "
+        + str(find_phm(results["ac"].freq, 2 * results["ac"].data["v(xtop.sig_out)"]))
+    )
+    print("Ivdd:            " + str(find_I_vdd(results["ac"].data["i(v.xtop.vvdc)"])))
 
-    print("Gain:            "+str(find_dc_gain(2*results["ac"].data["v(xtop.sig_out)"])))
-    print("UGBW:            "+str(find_ugbw(results["ac"].freq,2*results["ac"].data["v(xtop.sig_out)"])))
-    print("Phase margin:    "+str(find_phm(results["ac"].freq,2*results["ac"].data["v(xtop.sig_out)"])))
-    print("Ivdd:            "+str(find_I_vdd(results["ac"].data["i(v.xtop.vvdc)"])))
 
-def find_I_vdd(vout:numpy.array) -> float:
+def find_I_vdd(vout: numpy.array) -> float:
     return numpy.abs(vout)[0]
 
-def find_dc_gain(vout:numpy.array) -> float:
+
+def find_dc_gain(vout: numpy.array) -> float:
     return numpy.abs(vout)[0]
 
-def find_ugbw(freq:numpy.array, vout:numpy.array) -> float:
+
+def find_ugbw(freq: numpy.array, vout: numpy.array) -> float:
     gain = numpy.abs(vout)
     ugbw_index, valid = _get_best_crossing(gain, val=1)
     if valid:
@@ -209,7 +223,8 @@ def find_ugbw(freq:numpy.array, vout:numpy.array) -> float:
     else:
         return freq[0]
 
-def find_phm(freq:numpy.array, vout:numpy.array) -> float:
+
+def find_phm(freq: numpy.array, vout: numpy.array) -> float:
     gain = numpy.abs(vout)
     phase = numpy.angle(vout, deg=False)
     phase = numpy.unwrap(phase)  # unwrap the discontinuity
@@ -224,14 +239,15 @@ def find_phm(freq:numpy.array, vout:numpy.array) -> float:
     else:
         return -180
 
-def _get_best_crossing(yvec:numpy.array, val:float) -> tuple[int, bool]:
-    zero_crossings = numpy.where(numpy.diff(numpy.sign(yvec-val)))[0]
-    if len(zero_crossings)==0:
+
+def _get_best_crossing(yvec: numpy.array, val: float) -> tuple[int, bool]:
+    zero_crossings = numpy.where(numpy.diff(numpy.sign(yvec - val)))[0]
+    if len(zero_crossings) == 0:
         return 0, False
-    if abs((yvec-val)[zero_crossings[0]]) < abs((yvec-val)[zero_crossings[0]+1]):
+    if abs((yvec - val)[zero_crossings[0]]) < abs((yvec - val)[zero_crossings[0] + 1]):
         return zero_crossings[0], True
     else:
-        return (zero_crossings[0]+1), True
+        return (zero_crossings[0] + 1), True
 
 
 if __name__ == "__main__":
