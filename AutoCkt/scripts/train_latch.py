549c0da3f89c9eb2c89f79cb195683557177e6ee
Han Yan <yanh41@live.com>
2023-08-29 09:12:53 +0000

    updated LDO and OpAmp with negative gm

--- AutoCkt/scripts/train_latch.py
+++ AutoCkt/scripts/train_latch.py
@@ -15,23 +15,25 @@ from AutoCkt.Ckt.eval_engines.rewards import latch_reward
 
 circuit_optimization = AutoCktCircuitOptimization(
     params=AutoCktParams(
-        # TODO Change these
         [
-            AutoCktParam("mp1", (1, 100), step=1, init=34),
-            AutoCktParam("mn1", (1, 100), step=1, init=34),
-            AutoCktParam("mp3", (1, 100), step=1, init=34),
-            AutoCktParam("mn3", (1, 100), step=1, init=34),
-            AutoCktParam("mn4", (1, 100), step=1, init=34),
-            AutoCktParam("mn5", (1, 100), step=1, init=15),
-            AutoCktParam("cc", (0.1e-12, 10.0e-12), step=0.1e-12, init=2.1e-12),
+            AutoCktParam("w1", (1, 100), step=1, init=10),
+            AutoCktParam("w2", (1, 100), step=1, init=20),
+            AutoCktParam("w3", (1, 100), step=1, init=10),
+            AutoCktParam("w4", (1, 100), step=1, init=20),
+            AutoCktParam("w5", (1, 100), step=1, init=10),
+            AutoCktParam("w6", (1, 100), step=1, init=20),
+            AutoCktParam("w7", (1, 100), step=1, init=20),
+            AutoCktParam("w8", (1, 100), step=1, init=20),
+            AutoCktParam("w9", (1, 100), step=1, init=10),
+            AutoCktParam("w10", (1, 100), step=1, init=10),
         ]
     ),
     specs=AutoCktSpecs(
         # TODO Change these
         [
-            AutoCktSpec("gain", (200, 400), normalize=350),
-            AutoCktSpec("ugbw", (1.0e6, 2.5e7), normalize=9.5e5),
-            AutoCktSpec("phm", (60, 60.0000001), normalize=60),
+            AutoCktSpec("delay", (200, 400), normalize=350),
+            AutoCktSpec("setup_time", (1.0e6, 2.5e7), normalize=9.5e5),
+            AutoCktSpec("hold_time", (60, 60.0000001), normalize=60),
             AutoCktSpec("ibias", (0.0001, 0.01), normalize=0.001),
         ]
     ),
