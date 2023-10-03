4fab9183d31c3e891ddc5b67e265ecc6c4f23d58
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-10-03 23:45:10 +0000

    data copy over

--- AutoCkt/Shared/autockt_shared/opamp.py
+++ AutoCkt/Shared/autockt_shared/opamp.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 
 # Workspace Imports
 from discovery_shared import Rpc
+from pydantic import Field
 
 # Local Imports
 from .opamp_output import OpAmpOutput
@@ -22,13 +23,55 @@ class OpAmpInput:
     Input type for AutoCkt library, a state of result
     """
 
-    mp1: int  # number of units of specific pmos transistor
-    mn1: int  # number of units of specific nmos transistor
-    mp3: int  # number of units of specific pmos transistor
-    mn3: int  # number of units of specific nmos transistor
-    mn4: int  # number of units of specific nmos transistor
-    mn5: int  # number of units of specific nmos transistor
-    cc: float  # Or maybe `str`, or the Hdl21/ VLSIR `Prefixed` fixed-point type
+    mp1: int = Field(
+        description="number of units of specific pmos transistor",
+        default=34,
+        ge=1,
+        le=100,
+        step=1,
+    )
+    mn1: int = Field(
+        description="number of units of specific nmos transistor",
+        default=34,
+        ge=1,
+        le=100,
+        step=1,
+    )
+    mp3: int = Field(
+        description="number of units of specific pmos transistor",
+        default=34,
+        ge=1,
+        le=100,
+        step=1,
+    )
+    mn3: int = Field(
+        description="number of units of specific nmos transistor",
+        default=34,
+        ge=1,
+        le=100,
+        step=1,
+    )
+    mn4: int = Field(
+        description="number of units of specific nmos transistor",
+        default=34,
+        ge=1,
+        le=100,
+        step=1,
+    )
+    mn5: int = Field(
+        description="number of units of specific nmos transistor",
+        default=15,
+        ge=1,
+        le=100,
+        step=1,
+    )
+    cc: float = Field(
+        description="capacitance of specific capacitor",
+        default=2.1e-12,
+        ge=0.1e-12,
+        le=10.0e-12,
+        step=0.1e-12,
+    )  # Or maybe `str`, or the Hdl21/ VLSIR `Prefixed` fixed-point type
 
 
 @dataclass
@@ -36,10 +79,30 @@ class OpAmpOutput:
     """# Op-Amp Output
     Server output type reused by several op-amp flavors"""
 
-    gain: float
-    ugbw: float
-    phm: float
-    ibias: float
+    gain: float = Field(
+        description="gain of the op-amp",
+        ge=200,
+        le=400,
+        normalize=350,
+    )
+    ugbw: float = Field(
+        description="unity gain bandwidth of the op-amp",
+        ge=1.0e6,
+        le=2.5e7,
+        normalize=9.5e5,
+    )
+    phm: float = Field(
+        description="phase margin of the op-amp",
+        ge=60,
+        le=60.0000001,
+        normalize=60,
+    )
+    ibias: float = Field(
+        description="bias current of the op-amp",
+        ge=0.0001,
+        le=0.01,
+        normalize=0.001,
+    )
 
 
 auto_ckt_sim = Rpc(
