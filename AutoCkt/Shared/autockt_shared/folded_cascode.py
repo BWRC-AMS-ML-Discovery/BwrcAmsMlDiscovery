77d61b9faff636a46dac2261f9d48dae81b756df
Dan Fritchman <dan@fritch.mn>
2023-11-08 00:58:16 +0000

    Fcasc with Kings pydantic.Field based definitions

--- AutoCkt/Shared/autockt_shared/folded_cascode.py
+++ AutoCkt/Shared/autockt_shared/folded_cascode.py
@@ -1,60 +1,60 @@
 # Std-Lib Imports
-from dataclasses import dataclass
+from pydantic.dataclasses import dataclass, Field
 
 # Workspace Imports
 from discovery_shared import Rpc
 
 # Local Imports
 from .opamp_output import OpAmpOutput
-from .cktopt import (
-    CircuitOptimization,
-    MetricSpec,
-    MetricSpecs,
-    ParamSpec,
-    ParamSpecs,
-    Number,
-)
+from .rewards import settaluri_reward
+from .typing import as_param_specs
+from .cktopt import CircuitOptimization, MetricSpec, MetricSpecs
+
+
+def intparam(desc: str, default: int = 4) -> Field:
+    """# Positive integer-valued parameter"""
+    return Field(
+        description=desc,
+        default=default,
+        ge=1,
+        le=100,
+        step=1,
+    )
 
 
 @dataclass
 class FoldedCascodeInput:
-    w1_2: int
-    w5_6: int
-    w7_8: int
-    w9_10: int
-    w11_12: int
-    w13_14: int
-    w15_16: int
-    w17: int
-    w18: int
+    """# Folded Cascode Input
+    Generator parameters and optimization ranges for the `Fcasc` generator."""
 
-    cl: int
-    cc: int
-    rc: int
+    # Unit device sizes
+    nbias: int = intparam("Bias Nmos Unit Width", 2)
+    pbias: int = intparam("Bias Pmos Unit Width", 4)
+    ncasc: int = intparam("Cascode Nmos Unit Width", 2)
+    pcasc: int = intparam("Cascode Pmos Unit Width", 4)
+    ninp: int = intparam("Input Nmos Unit Width", 2)
+    pinp: int = intparam("Input Pmos Unit Width", 4)
 
-    wb0: int
-    wb1: int
-    wb2: int
-    wb3: int
-    wb4: int
-    wb5: int
-    wb6: int
-    wb7: int
-    wb8: int
-    wb9: int
-    wb10: int
-    wb11: int
-    wb12: int
-    wb13: int
-    wb14: int
-    wb15: int
-    wb16: int
-    wb17: int
-    wb18: int
-    wb19: int
+    # Current Mirror Ratios
+    alpha: int = intparam("Alpha (Pmos Input) Current Ratio", 2)
+    beta: int = intparam("Beta (Nmos Input) Current Ratio", 2)
+    gamma: int = intparam("Gamma (Output Cascode) Current Ratio", 2)
 
-    ibias: int
-    Vcm: int
+    # Other
+    vcb: int = Field(
+        description="Cascode Bias Voltage (mV)",
+        default=200,
+        ge=50,
+        le=350,
+        step=10,
+    )
+    cc: int = Field(
+        description="Load/ Compensation Cap Value (fF)",
+        default=1000,
+        ge=10,
+        le=10_000,
+        step=10,
+    )
 
 
 folded_cascode_sim = Rpc(
@@ -65,76 +65,8 @@ folded_cascode_sim = Rpc(
 )
 
 
-def folded_cascode_reward(
-    curr_output: OpAmpOutput,  # This is from simulation
-    target_output: dict[str, Number],  # This is from Specs
-):
-    """
-    TODO Implement
-    """
-
-    def calc_relative(curr: Number, ideal: Number):
-        # ideal = float(ideal)  # Not sure if this is necessary
-        relative = curr / ideal
-        return relative
-
-    # adapted TwoAmp reward using new variables
-    def reward(curr_output, target_output):
-        # populate relative for each key input of target
-        pos_val = []
-        reward = 1.0
-        for key in target_output:
-            reward = reward * calc_relative(
-                getattr(curr_output, key),
-                target_output[key],
-            )
-
-        # TODO this 10 seems pretty arbitrary
-        return reward
-
-    # run the reward function
-    return reward(curr_output, target_output)
-
-
 circuit_optimization = CircuitOptimization(
-    params=ParamSpecs(
-        [
-            ParamSpec("w1_2", (2, 24), step=2, init=10),
-            ParamSpec("w5_6", (2, 24), step=2, init=10),
-            ParamSpec("w7_8", (2, 24), step=2, init=10),
-            ParamSpec("w9_10", (2, 24), step=2, init=10),
-            ParamSpec("w11_12", (2, 24), step=2, init=10),
-            ParamSpec("w13_14", (2, 24), step=2, init=10),
-            ParamSpec("w15_16", (2, 24), step=2, init=10),
-            ParamSpec("w17", (2, 24), step=2, init=10),
-            ParamSpec("w18", (2, 24), step=2, init=10),
-            ParamSpec("cl", (8e-15, 30e-15), step=2e-15, init=10e-15),
-            ParamSpec("cc", (2e-15, 24e-15), step=2e-15, init=10e-15),
-            ParamSpec("rc", (0.6e3, 1.7e3), step=0.1e3, init=1e3),
-            ParamSpec("wb0", (2, 24), step=2, init=10),
-            ParamSpec("wb1", (2, 24), step=2, init=10),
-            ParamSpec("wb2", (2, 24), step=2, init=10),
-            ParamSpec("wb3", (2, 24), step=2, init=10),
-            ParamSpec("wb4", (2, 24), step=2, init=10),
-            ParamSpec("wb5", (2, 24), step=2, init=10),
-            ParamSpec("wb6", (2, 24), step=2, init=10),
-            ParamSpec("wb7", (2, 24), step=2, init=10),
-            ParamSpec("wb8", (2, 24), step=2, init=10),
-            ParamSpec("wb9", (2, 24), step=2, init=10),
-            ParamSpec("wb10", (2, 24), step=2, init=10),
-            ParamSpec("wb11", (2, 24), step=2, init=10),
-            ParamSpec("wb12", (2, 24), step=2, init=10),
-            ParamSpec("wb13", (2, 24), step=2, init=10),
-            ParamSpec("wb14", (2, 24), step=2, init=10),
-            ParamSpec("wb15", (2, 24), step=2, init=10),
-            ParamSpec("wb16", (2, 24), step=2, init=10),
-            ParamSpec("wb17", (2, 24), step=2, init=10),
-            ParamSpec("wb18", (2, 24), step=2, init=10),
-            ParamSpec("wb19", (2, 24), step=2, init=10),
-            ParamSpec("ibias", (30e-6, 52e-6), step=2e-6, init=40e-6),
-            ParamSpec("Vcm", (0, 1.2), step=0.1, init=1),
-        ]
-    ),
+    params=as_param_specs(FoldedCascodeInput),
     specs=MetricSpecs(
         [
             MetricSpec("gain", (200, 400), normalize=350),
@@ -146,5 +78,5 @@ circuit_optimization = CircuitOptimization(
     input_type=FoldedCascodeInput,
     output_type=OpAmpOutput,
     simulation=folded_cascode_sim,
-    reward=folded_cascode_reward,
+    reward=settaluri_reward,
 )
